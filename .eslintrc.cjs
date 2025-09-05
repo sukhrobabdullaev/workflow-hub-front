3d66c71681bff0fa3a3d66219fd103b3cf5fe2f6
Sukhrob Abdullaev <sukhrobabdullaev@kookmin.ac.kr>
2025-09-05 15:32:30 +0900

    fix: eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: ['eslint:recommended'],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'prefer-const': 'error',
+    'no-var': 'error',
+  },
+};
